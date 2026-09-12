1ec8ffcc6e7a52f6fc81651c6dcbcda64ef2fa3c
Kaka Abdeel Parubak <kakaparubak0420@gmail.com>
2026-09-12 19:29:40 +0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for (auto& str : strs) {
+            encoded += "_" + to_string(str.length()) + "_";
+            encoded += str;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> strs;
+        for (size_t i = 0; i < s.size(); i++) {
+            if (s[i] == '_') {
+                size_t j = i + 1;
+                bool is_not_len = false;
+                while (j < s.size() && s[j] != '_' ) {
+                    if (!isdigit(s[j])) {
+                        is_not_len = true;
+                        break;
+                    }
+                    j++;
+                }
+                if (is_not_len) continue;
+                int str_len = stoi(s.substr(i + 1, j - i - 1));
+                strs.push_back(s.substr(j + 1, str_len));
+                i = j; 
+            }
+        }
+        return strs;
+    }
+};
